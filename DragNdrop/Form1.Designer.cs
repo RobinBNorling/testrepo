a44de971b8fe963161c47173d4bee8dafd5a5545
Milice <robin.norling@skola.sala.se>
2021-12-01 09:09:14 +0100

    First commit
    Uploading all of my sheit

---
+++ DragNdrop/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace DragNdrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trash = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 192);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // trash
+            // 
+            this.trash.BackColor = System.Drawing.Color.LawnGreen;
+            this.trash.Location = new System.Drawing.Point(833, 407);
+            this.trash.Name = "trash";
+            this.trash.Size = new System.Drawing.Size(148, 166);
+            this.trash.TabIndex = 1;
+            this.trash.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "Button",
+            "PictureBox",
+            "CheckBox",
+            "Label"});
+            this.listBox1.Location = new System.Drawing.Point(821, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(160, 144);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(821, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 585);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trash);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trash)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox trash;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DragNdrop/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragNdrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool dragging = false;
+        Point dragStart = new Point(0, 0);
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragStart = e.Location;
+            dragging = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+            if(((Control)sender).Bounds.IntersectsWith(trash.Bounds))
+            {
+                Controls.Remove((Control)sender);
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(dragging)
+            {
+                ((Control)sender).Location = new Point(PointToClient(Cursor.Position).X - dragStart.X, PointToClient(Cursor.Position).Y - dragStart.Y);
+                Refresh();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control newControl = null;
+
+            if (listBox1.SelectedIndex == 0)
+            {
+                newControl = new Button();
+                newControl.Size = new Size(100, 25);
+                newControl.Text = "Button";
+            }
+            else if (listBox1.SelectedIndex == 1)
+            {
+                newControl = new PictureBox();
+                newControl.Size = new Size(128, 128);
+                newControl.BackColor = Color.DarkCyan;
+            }
+            else if (listBox1.SelectedIndex == 2)
+            {
+                newControl = new CheckBox();
+                newControl.Size = new Size(100, 25);
+                newControl.Text = "CheckBox";
+                newControl.BackColor = Color.Lime;
+            }
+            else if (listBox1.SelectedIndex == 3)
+            {
+                newControl = new Label();
+                newControl.Size = new Size(100, 25);
+                newControl.Text = "Label";
+                newControl.BackColor = Color.Lime;
+            }
+
+            newControl.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
+            newControl.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+            newControl.MouseUp += new MouseEventHandler(pictureBox1_MouseUp);
+
+            newControl.Location = new Point(15, 15);
+            Controls.Add(newControl);
+            newControl.BringToFront();
+        }
+    }
+}
